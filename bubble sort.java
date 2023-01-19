50e69308c8243b0cd7bae8fce89ed91a12a0bc09
Archit Sangal <76487258+architsangal-1729@users.noreply.github.com>
2023-01-20 00:13:22 +0530

    Create bubble sort.java
    using flag to make the the code optimised so that in best cae its time comexity becomes O(n) instead of O(n^2) before.

---
+++ bubble sort.java
@@ -0,0 +1,18 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {    
+    public static void bubbleSort(int[] arr, int n) {   
+        for(int i=0;i<=n-1;i++){
+            boolean flag = false;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    flag = true;
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        if(flag == false) break;
+        }
+    }
+}
