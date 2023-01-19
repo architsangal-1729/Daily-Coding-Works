d2b77933da592613eca563f0062cb1be617447fb
Archit Sangal <76487258+architsangal-1729@users.noreply.github.com>
2023-01-20 02:40:49 +0530

    Create insertion sort .java

---
+++ insertion sort .java
@@ -0,0 +1,18 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static void insertionSort(int n , int[] arr) {
+		// Write your code here.
+        for(int i=1;i<n;i++){
+            int temp = arr[i];
+            int j = i-1;
+            while(j>=0){
+                if(arr[j]>temp)
+                    arr[j+1]=arr[j];
+                else break;
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
+}
