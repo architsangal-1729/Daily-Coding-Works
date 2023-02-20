50581037d93e2022c0450425af66eb0a1b4caf04
Archit Sangal <76487258+architsangal-1729@users.noreply.github.com>
2023-02-20 23:48:39 +0530

    strivers dsa

---
+++ binary search.java
@@ -0,0 +1,20 @@
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int s=0;
+        int e=n-1;
+        int mid=-1;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(k==arr[mid]){
+                return mid;
+            }
+            else if(k<arr[mid]){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return -1;
+    }
+}
