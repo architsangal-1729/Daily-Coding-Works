c2965f7a57bb11b019d4147ba32ed93bbe2a7286
Archit Sangal <76487258+architsangal-1729@users.noreply.github.com>
2023-03-05 02:58:23 +0530

    Strivers DSA
    https://leetcode.com/problems/kth-missing-positive-number/solutions/?orderBy=most_votes

---
+++ 29.Kth Missing Positive Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low=0;
+        int high=arr.length;
+        while(low<high){
+            int mid=(low+high)>>1;
+            if(arr[mid]-mid-1>=k) high=mid;
+            else low=mid+1;
+        }
+        return low+k;
+    }
+}
