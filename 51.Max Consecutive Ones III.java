53f68550360f4a8a6cd543b7590734a987dd2cdb
Archit Sangal <76487258+architsangal-1729@users.noreply.github.com>
2023-03-18 01:16:35 +0530

    Strivers DSA
    https://leetcode.com/problems/max-consecutive-ones-iii/

---
+++ 51.Max Consecutive Ones III.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int i=0;
+        int j=0;
+        while(j<nums.length){
+            if(nums[j]==0) k--;
+            if(k<0 && nums[i++]==0) k++;
+            j++;
+        }
+        return j-i;
+    }
+}
