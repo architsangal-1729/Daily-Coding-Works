51dbcc621052f2dc461033f7789d5366c1e9264d
Archit Sangal <76487258+architsangal-1729@users.noreply.github.com>
2023-03-22 12:36:29 +0530

    Strivers DSA
    FOR LEETCODE--> https://leetcode.com/problems/max-consecutive-ones/
    FOR GFG--> https://practice.geeksforgeeks.org/problems/maximize-number-of-1s0905/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=maximize-number-of-1s

--- 48.Max Consecutive Ones.java
+++ 48.Max Consecutive Ones.java
@@ -22,3 +22,26 @@ public class Solution {
         return maxSum;
     }
 }
+
+/////////////////////////////////////   Find the maximum number of consecutive 1's produced by flipping at most M 0's   ///////////////////////////////////////////////
+class Solve {
+    // m is maximum of number zeroes allowed to flip
+    int findZeroes(int arr[], int n, int m) {
+        int i=0,j=0;
+        int count=0;
+        int maxLen = 0;
+        while(j<n){
+                if(count<=m){
+                    if(arr[j]==0) count++;
+                    maxLen = Math.max(maxLen, j-i);
+                    j++;
+                }
+                else{
+                    if(arr[i]==0) count--;
+                    i++;
+                }
+        }
+        if(count<=m) maxLen = Math.max(maxLen,j-i);
+        return maxLen;   
+    }
+}
